dc60dc74db151d662a0b5bdba0cfdffd6bc23a02
Kateryna Gotsuliak <k.gotsuliak@gmail.com>
2017-01-25 22:12:11 +0200

    added raise_application_error

--- dog/SQL/insert_new_breed_function.sql
+++ dog/SQL/insert_new_breed_function.sql
@@ -1,7 +1,9 @@
 CREATE OR REPLACE FUNCTION insert_new_breed (v_breed_name IN VARCHAR2, lang CHAR := 'E') return NUMBER
 IS
+invalid_lang EXCEPTION;
+PRAGMA EXCEPTION_INIT (invalid_lang, -20001); 
 BEGIN
-  CASE lang 
+  CASE UPPER(lang) 
     WHEN 'E' THEN
                  INSERT INTO breeds(breed_id, breed_name_eng)
                  VALUES(breed_id_seq.nextval ,v_breed_name);
@@ -11,8 +13,9 @@ BEGIN
     WHEN 'R' THEN 
                  INSERT INTO breeds(breed_id, breed_name_rus)
                  VALUES(breed_id_seq.nextval ,v_breed_name);
+    ELSE 
+      RAISE_APPLICATION_ERROR( -20001, 'Incorrect lang value "'||lang||'" The lang value should be in "E|U|R"');
   END CASE;
-  
   COMMIT;
   RETURN breed_id_seq.currval;   
 END;
